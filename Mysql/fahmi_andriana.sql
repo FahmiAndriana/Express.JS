274ba5e9fe50473f93c84b2f2581cc60bd1778f0
FahmiAndriana <94207375+FahmiAndriana@users.noreply.github.com>
2021-12-29 09:10:22 +0700

    Add files via upload

---
+++ Mysql/fahmi_andriana.sql
@@ -0,0 +1,6 @@
+insert into `fahmi_andriana` (`Gaji`, `Nama`, `Tanggal_Lahir`, `id`) values (12000000, 'Fahmi Andriana', '1994-06-05', 1012);
+insert into `fahmi_andriana` (`Gaji`, `Nama`, `Tanggal_Lahir`, `id`) values (11000000, 'Saepudin', '1990-12-12', 1013);
+insert into `fahmi_andriana` (`Gaji`, `Nama`, `Tanggal_Lahir`, `id`) values (12000000, 'Andriana', '1989-06-15', 1014);
+insert into `fahmi_andriana` (`Gaji`, `Nama`, `Tanggal_Lahir`, `id`) values (11000000, 'Sulaiman', '1988-06-25', 1016);
+insert into `fahmi_andriana` (`Gaji`, `Nama`, `Tanggal_Lahir`, `id`) values (14000000, 'Fahmi Andriana', '1989-07-30', 1017);
+;
